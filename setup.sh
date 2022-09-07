a80eabaf52b529ec391c64267ec93497ca450e01
Fulano de Tal <jean.lk@hotmail.com>
2022-09-07 11:45:47 -0300

    v0.0.2

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ email = \"jeanlkmota.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
-headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+headless = true\n\
+\n\
 " > ~/.streamlit/config.toml
