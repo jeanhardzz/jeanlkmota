93071da1a9bcd6e34782cda0a90e5df7185e0789
Fulano de Tal <jean.lk@hotmail.com>
2022-09-07 12:01:19 -0300

    v0.0.5

--- main.py
+++ main.py
@@ -36,14 +36,22 @@ select = option_menu(
     default_index=0,    
     orientation="horizontal",
     styles={
+            "container": {"padding": "0!important", "background-color": "#fafafa"},
+            "icon": {"color": "orange", "font-size": "25px"}, 
+            "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+            "nav-link-selected": {"background-color": "green"},
+    }
+)
+
+"""
+styles={
         "container": {"border": "solid 1px rgba(160, 160, 160, 0.3)","padding": "5!important", "background-color": "#f4f4f4"},
         "icon": {"font-size": "0px"}, 
         "nav-link": {"font-size": "16px", "text-align": "center", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {},
                 
     }
-)
-
+"""
 
 #MENU LATERAL
 with st.sidebar:
