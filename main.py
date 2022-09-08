30b729ac361b4178c77525fe4a53d307478748e2
Fulano de Tal <jean.lk@hotmail.com>
2022-09-08 12:41:33 -0300

    vpages8

--- main.py
+++ main.py
@@ -24,9 +24,9 @@ select = option_menu(
     default_index=0,    
     orientation="horizontal",
     styles={
-        "container": {"color": "#333333","border": "solid 1px rgba(160, 160, 160, 0.3)","padding": "0!important", "background-color": "#f4f4f4"},
+        "container": {"color": "#333333","padding": "0!important", "background-color": "#f4f4f4"},
         "icon": {"font-size": "0px"}, 
-        "nav-link": {"font-size": "16px", "text-align": "center", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link": {"color": "#333333","font-size": "16px", "text-align": "center", "margin":"0px", "--hover-color": "#eee"},
         "nav-link-selected": {},
                 
     }
